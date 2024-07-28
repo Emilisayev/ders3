f28c258fcfa6866108094d647cc035ba2b582707
Emilisayev <eisayev54@gmail.com>
2024-07-28 23:42:53 +0400

    first commit

---
+++ lesson.dart
@@ -0,0 +1,111 @@
+void main(List<String> args) {
+  // print("starting");
+
+  // print(2+5);
+
+  // int age = 15;
+  // double xas = 25.5;
+  // bool isivarmi = false;
+  // String name = "Emil";
+
+  // print (age);
+  // print(xas);
+  // print(isivarmi);
+  // print(name);
+
+// int a = 25;
+// int b = 55;
+// print(a+b);
+
+// int a = 25;
+// int b = 55;
+// print(b-a);
+// print(a==b);
+// print(a>=b);
+// print(a<=b);
+
+// String student1 = "Emil";
+// String student2 = "Elvin";
+// String student3 = "Resul";
+// print(student1+" "+ student2+ " " +student3);
+// print(student1+"\n"+ student2+ "\n" + student3);
+
+// int a = 5;
+// int b = 7;
+// int c = 8;
+// int d = 9;
+
+// print('Toplama: ${a+b}');
+// print('Cixma: ${a-b}');
+// print('vurma: ${a*b}');
+// int a = 5;
+
+// if (a>=5){
+//   print("a boyukdur 5den");
+// } 
+// else {
+//   print("a kicikdir 5den");
+// }
+
+// bool isivarmi = true;
+
+// if (isivarmi==false) {
+//   print("emilin isi var");
+
+//   }
+//   else {
+//     print("emilin isi yoxdu");
+//   }
+
+ int a = 6;
+
+ if (a>=0) {
+   print(" a is positive");
+ }else if (a==0) {
+   print("a is 0");
+ }
+  else{
+   print("a is negative");
+ }
+
+ int age = 29;
+
+if (age >= 0 && age <= 12) {
+  print("child");
+} 
+else if (age >= 13 && age <= 19) {
+  print("Teenager");
+} 
+else if (age >= 20 && age <= 65) {
+  print("Adult");
+} 
+else if (age > 65) {
+  print("old");
+} 
+
+
+int temp = 11;
+
+if (temp<0) {
+  print("frosty");
+} 
+else if (temp >= 0 && temp <= 20) {
+  print("cold");
+} 
+else if (temp >= 21 && temp <= 30) {
+  print("warm");
+} 
+else if (temp > 30) {
+  print("hot");
+} 
+
+int password =12345;
+
+if (password==12345){
+  print("True");
+} else{
+  print("incorrect");
+}
+
+
+}
